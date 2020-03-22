d06614e0a907126366939d691474512114674982
hoangkh <hoangkhoa_90@yahoo.com>
2020-03-22 23:52:41 +0100

    media-header added

--- src/components/media-entertain/Footer.js
+++ src/components/media-entertain/Footer.js
@@ -3,7 +3,22 @@ import React from 'react';
 
 function Footer() {
     return <div id="shop-page-footer">
-      <h2>This is E-commerce Footer</h2>
+      <div className="row useful-links">
+        <div className="col-6 list-links">
+          <div className="row">
+    
+            </div>
+        </div>
+        <div className="col-6">
+            <div className="row">
+              <div className="col-6"><h3>Sign up for news</h3></div>
+              <div className="col-6"><h3>Rating</h3></div>
+            </div>
+        </div>
+      </div>
+      <div className="row copy-rights">This is where copy rights
+
+      </div>
       </div>;
   }
 
--- src/components/media-entertain/Header.js
+++ src/components/media-entertain/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+  Link
+} from "react-router-dom";
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 
 function Header() {
     return <div id="media-page-header">
-      <h2>This is E-commerce Footer</h2>
+      <Navbar expand="lg">
+        <Link to="/shop" className="brand-name">My Shop</Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link className="menu-items" to="/shop">Men</Link>
+            <Link className="menu-items" to="/shop">Woman</Link>
+            <Link className="menu-items" to="/shop">Kids</Link>
+            <Link className="menu-items" to="/shop">Accessories</Link>
+            <Link className="menu-items" to="/shop">Brands</Link>
+            <Link className="menu-items" to="/shop">Sales</Link>
+            <Link className="menu-items" to="/shop">My basket</Link>
+          </Nav>
+          <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+        </Navbar.Collapse>
+      </Navbar>
       </div>;
   }
 
